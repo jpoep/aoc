1b8f3bca9f1b7a3e9c00fc88920ab5bb6c3718a0
Jonas Pöppelmann <jonas.poeppelmann@stf-gruppe.de>
2024-12-01 12:10:18 +0100

    Implement part 2

--- 2024/1/part1.hs
+++ 2024/1/part1.hs
@@ -10,7 +10,6 @@ convertToList = splitOn "\n"
 splitEntries :: [String] -> [(Int, Int)]
 splitEntries list = [(read (head (words x)) :: Int, read (last (words x)) :: Int) | x <- list, not $ null x]
 
-
 separateLists :: [(Int, Int)] -> ([Int], [Int])
 separateLists list = ([fst x | x <- list], [snd x | x <- list])
 
@@ -23,10 +22,18 @@ sortLists (list1, list2) = (sort list1, sort list2)
 getDifferences :: ([Int], [Int]) -> Int
 getDifferences input = sum [abs (x - y) | (x, y) <- uncurry zip input]
 
-getOutput = getDifferences . sortLists . parseInput
+getSimilarityScore :: ([Int], [Int]) -> Int
+getSimilarityScore input = sum [x * findOccurrences x (snd input) | x <- fst input]
+
+findOccurrences :: Int -> [Int] -> Int
+findOccurrences x list = length (filter (== x) list)
+
+part1 = getDifferences . sortLists . parseInput
+
+part2 = getSimilarityScore . parseInput
 
 main :: IO ()
 main = do
   content <- readFile "input.txt"
-  let output = getOutput content
-  putStrLn $ "The difference is: " ++ show output
+  putStrLn $ "Part 1 answer " ++ show (part1 content)
+  putStrLn $ "Part 2 answer " ++ show (part2 content)
